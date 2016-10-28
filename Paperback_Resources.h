1d1defeddca9984b495516716e2b6297f51cc9a2
Max Power <aspadistra@users.noreply.github.com>
2016-10-28 16:13:31 -0700

    Update Paperback_Resources.h

--- Paperback_Resources.h
+++ Paperback_Resources.h
@@ -1,4 +1,34 @@
 
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// PaperBack -- high density backups on the plain paper                       //
+//                                                                            //
+// Copyright (c) 2007 Oleh Yuschuk                                            //
+// ollydbg at t-online de (set Subject to 'paperback' or be filtered out!)    //
+//                                                                            //
+//                                                                            //
+// This file is part of PaperBack.                                            //
+//                                                                            //
+// Paperback is free software; you can redistribute it and/or modify it under //
+// the terms of the GNU General Public License as published by the Free       //
+// Software Foundation; either version 3 of the License, or (at your option)  //
+// any later version.                                                         //
+//                                                                            //
+// PaperBack is distributed in the hope that it will be useful, but WITHOUT   //
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      //
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   //
+// more details.                                                              //
+//                                                                            //
+// You should have received a copy of the GNU General Public License along    //
+// with this program. If not, see <http://www.gnu.org/licenses/>.             //
+//                                                                            //
+//                                                                            //
+// Note that bzip2 compression/decompression library, which is the part of    //
+// this project, is covered by different license, which, in my opinion, is    //
+// compatible with GPL.                                                       //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 #define M_FILE_OPEN    1001
 #define M_FILE_SAVEBMP 1002
 #define M_FILE_SELECT  1003
@@ -36,50 +66,3 @@
 #define PAS_OK         IDOK
 #define PAS_CANCEL     IDCANCEL
 
-////////////////////////////////////////////////////////////////////////////////
-//                                                                            //
-// PaperBack -- high density backups on the plain paper                       //
-//                                                                            //
-// Copyright (c) 2007 Oleh Yuschuk                                            //
-// ollydbg at t-online de (set Subject to 'paperback' or be filtered out!)    //
-//                                                                            //
-//                                                                            //
-// This file is part of PaperBack.                                            //
-//                                                                            //
-// Paperback is free software; you can redistribute it and/or modify it under //
-// the terms of the GNU General Public License as published by the Free       //
-// Software Foundation; either version 3 of the License, or (at your option)  //
-// any later version.                                                         //
-//                                                                            //
-// PaperBack is distributed in the hope that it will be useful, but WITHOUT   //
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      //
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for   //
-// more details.                                                              //
-//                                                                            //
-// You should have received a copy of the GNU General Public License along    //
-// with this program. If not, see <http://www.gnu.org/licenses/>.             //
-//                                                                            //
-//                                                                            //
-// Note that bzip2 compression/decompression library, which is the part of    //
-// this project, is covered by different license, which, in my opinion, is    //
-// compatible with GPL.                                                       //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
-
-ICON_MAIN ICON
-{
- '00 00 01 00 02 00 20 20 10 00 00 00 00 00 E8 02'
- '00 00 26 00 00 00 10 10 10 00 00 00 00 00 28 01'
- '00 00 0E 03 00 00 28 00 00 00 20 00 00 00 40 00'
- '00 00 01 00 04 00 00 00 00 00 80 02 00 00 00 00'
- '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
- '00 00 00 00 80 00 00 80 00 00 00 80 80 00 80 00'
- '00 00 80 00 80 00 80 80 00 00 80 80 80 00 C0 C0'
- 'C0 00 00 00 FF 00 00 FF 00 00 00 FF FF 00 FF 00'
- '00 00 FF 00 FF 00 FF FF 00 00 FF FF FF 00 00 00'
- '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'
- '00 FF FF FF FF FF FF FF FF FF FF 00 00 00 00 00'
- '00 FF 0F 0F 0F 0F 0F 0F 0F 0F 0F 00 00 00 00 00'
- '00 FF FF FF FF FF FF FF FF FF FF 00 00 00 00 00'
- '00 FF 0F 0F 0F 0F 0F 0F 0F 0F 0F 00 00 00 00 00'
- '00 FF FF FF FF FF FF FF FF FF FF 00 00 00 00 00'
